569641923f5ee40b7130055ca5b3dd3f729555b5
fedepenic <fedepenic@hotmail.com>
2021-11-09 21:27:18 -0300

    Manejamos mensajes entre cliente y servidor

--- serverPackage/main.go
+++ serverPackage/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
 )
 
 const (
-	connHost = "localhost" // Por ahora el programa funciona en el entorno local.
+	connHost = "localhost" // Por ahora el programa funciona en el entorno local, mas adelante se analizara si se extiende a que funcione entre diferentes equipos.
 	connPort = "8080"
 	connType = "tcp" // Protocolo de transmision de mensajes.
 )
@@ -27,7 +28,27 @@ func main() {
 			return
 		}
 
-		fmt.Println("Client " + client.RemoteAddr().String() + " connected.")
+		go handleConnection(client) // En esta parte manejamos los mensajes entre servidor y cliente.
 	}
 
+}
+
+func handleConnection(client net.Conn) {
+	buffer, err := bufio.NewReader(client).ReadBytes('\n') // Leo el mensaje del cliente, funcion bloqueante.
+
+	// Cierro las conexiones cuando el cliente se va.
+	if err != nil {
+		fmt.Println("Se fue el cliente.")
+		client.Close()
+		return
+	}
+
+	fmt.Println("Mensaje del Cliente:", string(buffer[:len(buffer)-1]))
+
+	// Le respondemos al cliente.
+	respuesta := []byte("Gracias por hablarle al server")
+	client.Write(respuesta)
+
+	// Repetimos el proceso hasta que el cliente se vaya.
+	handleConnection(client)
 }
