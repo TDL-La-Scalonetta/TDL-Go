7771c76a1c2ab1d1c173a0998998a4ec26083e29
Alfonso Campodonico <38387367+AlfonsoCampodonico@users.noreply.github.com>
2021-11-21 16:39:09 -0300

    Timer

--- user.go
+++ user.go
@@ -1,88 +1,88 @@
 package main
 
 import (
-  "log"
-  "encoding/json"
-  "github.com/gorilla/websocket"
+	"encoding/json"
+	"github.com/gorilla/websocket"
+	"log"
 )
 
 var upgrader = websocket.Upgrader{
-    ReadBufferSize:  4096,
-    WriteBufferSize: 4096,
+	ReadBufferSize:  4096,
+	WriteBufferSize: 4096,
 }
 
 type User struct {
-	Name 	 	  string            `json:"Name"`
-	Amount 	  float32           `json:"Amount"`
-	Conn      *websocket.Conn   `json:"-"`
-  Room      *Room             `json:"-"`
-  Sender    chan *RoomMessage `json:"-"`
+	Name   string            `json:"Name"`
+	Amount float32           `json:"Amount"`
+	Conn   *websocket.Conn   `json:"-"`
+	Room   *Room             `json:"-"`
+	Sender chan *RoomMessage `json:"-"`
 }
 
 type UserMessage struct {
-  Action  string
-  Offer   string
-  Room    string
-  User    *User
+	Action string
+	Offer  string
+	Room   string
+	User   *User
 }
 
 func newUser(name string) *User {
-  return &User{
-    Name:     name,
-    Amount: 	10000,
-    Sender:   make(chan *RoomMessage),
-  }
+	return &User{
+		Name:   name,
+		Amount: 10000,
+		Sender: make(chan *RoomMessage),
+	}
 }
 
 func (user *User) handleMessage(rawMessage []byte) {
-  var message UserMessage
-  if err := json.Unmarshal(rawMessage, &message); err != nil {
-      log.Printf("Error on unmarshal JSON message %s", err)
-  }
+	var message UserMessage
+	if err := json.Unmarshal(rawMessage, &message); err != nil {
+		log.Printf("Error on unmarshal JSON message %s", err)
+	}
 
-  switch message.Action {
-  case "offer":
-      message.User = user
-      user.Room.offers <-&message
-  case "leave":
-      user.Room.unregister <-user
-  }
+	switch message.Action {
+	case "offer":
+		message.User = user
+		user.Room.offers <- &message
+	case "leave":
+		user.Room.unregister <- user
+	}
 }
 
 func (user *User) disconnect() {
-  log.Println("Desconectarse")
+	log.Println("Desconectarse")
 }
 
 func (user *User) updateStatus(rM *RoomMessage) {
-  parsed, err1 := json.Marshal(rM)
-  if err1 != nil {
-      log.Println(err1)
-  }
+	parsed, err1 := json.Marshal(rM)
+	if err1 != nil {
+		log.Println(err1)
+	}
 
-  err2 := user.Conn.WriteMessage(websocket.TextMessage, parsed)
-  if err2 != nil {
-      log.Println(err2)
-  }
+	err2 := user.Conn.WriteMessage(websocket.TextMessage, parsed)
+	if err2 != nil {
+		log.Println(err2)
+	}
 }
 
 func (user *User) WriteSocket() {
-  for {
-			select {
-        case message := <-user.Sender:
-          user.updateStatus(message)
-      }
+	for {
+		select {
+		case message := <-user.Sender:
+			user.updateStatus(message)
+		}
 	}
 }
 
 func (user *User) ReadSocket() {
-    for {
-        _, rawMessage, err := user.Conn.ReadMessage()
-        if err != nil {
-            if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-                log.Printf("unexpected close error: %v", err)
-            }
-            break
-        }
-        user.handleMessage(rawMessage)
-    }
+	for {
+		_, rawMessage, err := user.Conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("unexpected close error: %v", err)
+			}
+			break
+		}
+		user.handleMessage(rawMessage)
+	}
 }
